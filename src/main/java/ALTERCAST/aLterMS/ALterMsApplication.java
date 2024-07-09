ccf26df95a63f6951e74e17d6070bb8bf875ee87
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-07-09 14:04:01 +0900

    init

---
+++ src/main/java/ALTERCAST/aLterMS/ALterMsApplication.java
@@ -0,0 +1,13 @@
+package ALTERCAST.aLterMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALterMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ALterMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ALTERCAST/aLterMS/ALterMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ALTERCAST.aLterMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ALterMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
